ae69ae987960bed2e58f33c3d5b11ac806409ef0
AdamJamesNaylor <adamnaylor@gmail.com>
2017-03-28 07:27:48 +0100

    asf

--- Dillon.Server/UI/Akira/script.js
+++ Dillon.Server/UI/Akira/script.js
@@ -1,28 +1,4 @@
-﻿function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-    touch.screenX, touch.screenY,
-    touch.clientX, touch.clientY, false,
-    false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
-
-function update(ul, e) {
+﻿function update(ul, e) {
     var height = ul.outerHeight();
     var pos = e.pageY - ul.offset().top;
     var percent = Math.floor(((height - pos) / height) * 100);
@@ -57,11 +33,8 @@ function update(ul, e) {
 }
 
 $(document).ready(function () {
-    $("body").requestFullscreen();
 
     update($('ul'), { pageY: 1120});
-
-    init();
 
     var down = false;
 
@@ -80,5 +53,9 @@ $(document).ready(function () {
         if (!down) return;
         var ul = $(this);
         update(ul, e);
+    });
+
+    $("button").bind("tap", function() {
+        alert('test');
     });
 });
