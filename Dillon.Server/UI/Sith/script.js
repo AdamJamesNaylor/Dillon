76ebb98a054fa2e41f15428e8f8a7d3c3d6cd1fa
AdamJamesNaylor <adamnaylor@gmail.com>
2017-04-14 00:12:47 +0100

    Add sith UI

---
+++ Dillon.Server/UI/Sith/script.js
@@ -0,0 +1,16 @@
+﻿$.fn.random = function () {
+    var ret = $();
+
+    if (this.length > 0)
+        ret = ret.add(this[Math.floor((Math.random() * this.length))]);
+
+    return ret;
+};
+
+function flashGrids() {
+    $("table.grid td").random().toggleClass("on");
+}
+
+$(document).ready(function () {
+    var gridTimer = setInterval(flashGrids, 3000);
+});
